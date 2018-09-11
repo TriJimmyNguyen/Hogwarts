ef48c27009c4371426e2cb600e1ebac7ce49c032
TriJimmy Nguyen <tjnguyen.school@gmail.com>
2018-09-11 11:55:33 -0400

    La taille des titres adjuster

--- store.php
+++ store.php
@@ -108,29 +108,33 @@ var_dump($_SESSION);
     <p>Select what kind of items you wish to view</p>
     <div id="search">
         <form method="get" action="<?= $_SERVER['PHP_SELF']?>">
-            <input type="checkbox" name="wands" value="wands"
-                <?php
-                     if(array_key_exists('wands', $_COOKIE) && $_COOKIE['wands'] == 'view_yes'){
-                         echo CHECKED_ATTR;
-                     }
-                 ?>
-            />wands<br>
-
-            <input type="checkbox" name="brooms" value="brooms"
-                <?php
-                    if(array_key_exists('brooms', $_COOKIE) && $_COOKIE['brooms'] == 'view_yes'){
-                        echo CHECKED_ATTR;
-                   }
-                ?>
-            />brooms<br>
-
-            <input type="checkbox" name="books" value="books"
-                <?php
-                    if(array_key_exists('books', $_COOKIE) && $_COOKIE['books'] == 'view_yes'){
-                        echo CHECKED_ATTR;
-                    }
-                ?>
-            />books<br>
+            <p>
+                <input type="checkbox" name="wands" value="wands"
+                    <?php
+                         if(array_key_exists('wands', $_COOKIE) && $_COOKIE['wands'] == 'view_yes'){
+                             echo CHECKED_ATTR;
+                         }
+                     ?>
+                />wands<br>
+            </p>
+            <p>
+                    <input type="checkbox" name="brooms" value="brooms"
+                        <?php
+                            if(array_key_exists('brooms', $_COOKIE) && $_COOKIE['brooms'] == 'view_yes'){
+                                echo CHECKED_ATTR;
+                           }
+                        ?>
+                    />brooms<br>
+            </p>
+            <p>
+                <input type="checkbox" name="books" value="books"
+                    <?php
+                        if(array_key_exists('books', $_COOKIE) && $_COOKIE['books'] == 'view_yes'){
+                            echo CHECKED_ATTR;
+                        }
+                    ?>
+                />books<br>
+            </p>
 
             <input type="submit" name="submitSearch"  value="view"/>
         </form>
