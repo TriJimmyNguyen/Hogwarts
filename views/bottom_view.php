648e8d61666227a5f8592c37d7016879d2dfe9fa
TriJimmy Nguyen <tjnguyen.school@gmail.com>
2018-09-13 11:50:56 -0400

    Enlever le bottom flair

--- views/bottom_view.php
+++ views/bottom_view.php
@@ -3,8 +3,6 @@
 ?>
 
             <footer>
-                <p id="bottomFlair">~</p>
-                <div id="bottomFlairLine"></div>
                 <?php require("views/menu.php");?>
             </footer>
         </div> <!--Wrapper-->
